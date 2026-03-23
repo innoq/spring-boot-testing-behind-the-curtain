fabd3cf422dca231e48dd6ed9aad20a9c5b4415e
Michael Vitz <vitz.michael@googlemail.com>
2026-03-23 16:57:18 +0100

    Migrate to existing Spring Framework stuff

--- src/test/java/com/innoq/talk/spring/test/GreeterIntegrationTests.java
+++ src/test/java/com/innoq/talk/spring/test/GreeterIntegrationTests.java
@@ -1,14 +1,17 @@
 package com.innoq.talk.spring.test;
 
-import com.innoq.talk.spring.test.DemoSpringExtension.TestProperty;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(DemoSpringExtension.class)
-@TestProperty(key = "greeting", value = "Hi %s!")
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = "greeting=Hi %s!")
 class GreeterIntegrationTests {
 
     @Autowired
@@ -20,5 +23,10 @@ class GreeterIntegrationTests {
 
         assertThat(greeting)
                 .isEqualTo("Hi all!");
+    }
+
+    @Configuration
+    @ComponentScan(basePackageClasses = Application.class)
+    static class TestConfig {
     }
 }
